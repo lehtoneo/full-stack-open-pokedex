9c04851e1681dfb33d0f5eaee81eb54f54220abb
lehtoneo <ossi.j.lehtonen@helsinki.fi>
2022-10-26 16:09:18 +0300

    real healthcheck script

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+URL="https://ossi-pokedex.fly.dev/health"
 
-exit 1 # exit status 1 means that the script "fails"
+# store the whole response with the status at the and
+HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X GET $URL)
+
+# extract the body
+HTTP_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
+
+# extract the status
+HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+
+# example using the status
+if [ ! $HTTP_BODY == "ok"  ]; then
+  echo "Error [HTTP body: $HTTP_BODY]"
+  echo "[HTTP status: $HTTP_STATUS]"
+  exit 1
+fi
+echo "STATUS: $HTTP_STATUS"
+echo "HEALTH CHECK OK"
